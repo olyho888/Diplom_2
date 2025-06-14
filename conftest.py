098320e74c1824a2740e2019bff0c896b110850e
olyho888 <olyho@yandex.ru>
2025-06-14 10:17:34 +0500

    Diplom_2 commit after review

--- conftest.py
+++ conftest.py
@@ -11,14 +11,12 @@ def user_create_fixture():
     user_methods.delete_user(token)
 
 @pytest.fixture
-def orders_create_fixture():
+def orders_create_fixture(user_create_fixture):
     numbers = []
-    user_methods = UserMethods()
-    _, token = user_methods.create_user_successful()
+    _, token = user_create_fixture
     order_methods = OrderMethods()
     params = {'ingredients': order_methods.get_ingredients()}
     for i in range(3):
         number, _ = order_methods.create_order_with_auth_successful(params, token)
         numbers.append(number)
-    yield token, numbers
-    user_methods.delete_user(token)
+    return token, numbers
